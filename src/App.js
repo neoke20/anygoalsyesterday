050f205988ebd46f5ef54b0eef80a32910d77ec7
Kevin Konczak <konczakkevin@gmail.com>
2022-01-13 22:22:44 +0900

    styled the site title h1 and h2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
   return (
     <Router>
       <Link className="text-decoration-none" to="/">
-        <h1 className="text-center">Any Goals?</h1>
+        <h1 className="text-center">Any<span id='title-g'>Goals</span></h1>
+        <h2 className="text-center" id='title-y'>Yesterday?</h2>
       </Link>
       <Routes>
         <Route path="/competition/:id" element={<Competition />} />
