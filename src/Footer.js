81e06704ff0919e1a4d880fa59d14044e9967a3e
Kevin Konczak <konczakkevin@gmail.com>
2022-01-15 15:46:26 +0900

    Added social icons and made responsive

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,11 @@
 const Footer = () => {
   const today = new Date();
-  return (<div>
+  return (<div id="footer">
     <p>&copy; {today.getFullYear()} Kevin Konczak</p>
+    <ul className="list-inline">
+      <li className="list-inline-item"><a href="https://github.com/neoke20https://www.linkedin.com/in/konczakkevin/"><i class="fab fa-github"></i></a></li>
+      <li className="list-inline-item"><a href="https://www.linkedin.com/in/konczakkevin/"><i class="fab fa-linkedin"></i></a></li>
+    </ul>
   </div>)
 };
 
