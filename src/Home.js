45ac677f1a5cdf86ee119c40d32296533ae82a11
Kevin Konczak <konczakkevin@gmail.com>
2022-01-25 16:29:01 +0900

    changed the URL so it includes the latest matchday

--- src/Home.js
+++ src/Home.js
@@ -24,9 +24,10 @@ const Home = () => {
     );
     const competitions = document.querySelector("#competitions");
     allCompetitions.forEach((competition) => {
-      const competitionName = `<a class="competition-card" href="competition/${competition.code}" style="background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
+      const competitionName = `<a class="competition-card" href="competition/${competition.code}-matchday=${competition.currentSeason.currentMatchday}" style="background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
     url(${competition.area.ensignUrl});"><div class="d-flex flex-column"><h2>${competition.name}</h2><p class="country-name">(${competition.area.name})</p></div></a>`;
       competitions.insertAdjacentHTML("beforeend", competitionName);
+      console.log(competition);
     });
   });
 
