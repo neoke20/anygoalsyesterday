e9b4f5b09bbe82de20ccaac6f9f7e6120434e73d
Kevin Konczak <konczakkevin@gmail.com>
2022-01-27 22:45:27 +0900

    Changed the O in the logo for a football. Rotating

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.scss';
+import "./App.scss";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./Home";
 import Competition from "./Competition";
@@ -8,8 +8,15 @@ function App() {
   return (
     <Router>
       <Link className="text-decoration-none" to="/">
-        <h1 className="text-center">Any<span id='title-g'>Goals</span></h1>
-        <h2 className="text-center" id='title-y'>Yesterday?</h2>
+        <h1 className="text-center">
+          Any
+          <span id="title-g">
+            G<i className="far fa-futbol title-o rotating"></i>als
+          </span>
+        </h1>
+        <h2 className="text-center" id="title-y">
+          Yesterday?
+        </h2>
       </Link>
       <Routes>
         <Route path="/competition/:id" element={<Competition />} />
