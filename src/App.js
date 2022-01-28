023dd7ff698375fae424d2553919082d4537332d
Kevin Konczak <konczakkevin@gmail.com>
2022-01-28 13:31:44 +0900

    Changed the icon for an image, centered the rotation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./Home";
 import Competition from "./Competition";
 import Footer from "./Footer";
+import ball from "./football.png";
 
 function App() {
   return (
@@ -10,9 +11,9 @@ function App() {
       <Link className="text-decoration-none" to="/">
         <h1 className="text-center">
           Any
-          <span id="title-g">
-            G<i className="far fa-futbol title-o rotating"></i>als
-          </span>
+          <span className="title-g">G</span>
+          <img src={ball} alt="Ball" className="rotating title-o"></img>
+          <span className="title-als">als</span>
         </h1>
         <h2 className="text-center" id="title-y">
           Yesterday?
