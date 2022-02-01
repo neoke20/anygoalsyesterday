dd4a01ac8e03cfd9a929c46de05be481eca19082
Kevin Konczak <konczakkevin@gmail.com>
2022-02-01 13:07:54 +0900

    Created hook to get all squad members

--- src/Team.js
+++ src/Team.js
@@ -6,6 +6,7 @@ const teamID = url.match(/(\d+)(?!.*\d)/);
 
 const Team = () => {
   const [team, setTeam] = useState("");
+  const [squad, setSquad] = useState([]);
 
   useEffect(() => {
     requestTeamInfo();
@@ -23,19 +24,24 @@ const Team = () => {
     const json = await res.json();
     console.log(json);
     setTeam(json);
+    setSquad(json.squad);
   }
   return (
-    <div className="text-center text-white team-card">
-      <img src={team.crestUrl} alt="logo" height={120} />
-      <h2>{team.name}</h2>
-      <p>Founded in {team.founded}</p>
-      <p>Venue: {team.venue}</p>
-      <p>
-        Visit{" "}
-        <a href={team.website} target="_blank" rel="noreferrer">
-          team's website
-        </a>
-      </p>
+    <div>
+      <div className="text-center text-white team-card">
+        <img src={team.crestUrl} alt="logo" height={120} />
+        <h2>{team.name}</h2>
+        <p>{team.address}</p>
+        <p>Founded in {team.founded}</p>
+        <p>Venue: {team.venue}</p>
+        <p>
+          Visit{" "}
+          <a href={team.website} target="_blank" rel="noreferrer">
+            team's website
+          </a>
+        </p>
+        <p>{squad.map((player) => player.name)}</p>
+      </div>
     </div>
   );
 };
