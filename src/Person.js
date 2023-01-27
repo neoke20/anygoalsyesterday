ad939c002203627d1d3053b4af3c8240d76f77c6
Kevin Konczak <konczakkevin@gmail.com>
2023-01-27 17:04:43 +0900

    Added a page to have player info

--- src/Person.js
+++ src/Person.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 
 const Person = () => {
+  const [person, setPerson] = useState([]);
 
   useEffect(() => {
     requestMatches();
@@ -13,14 +13,16 @@ const Person = () => {
       headers: { "X-Auth-Token": "1d76b9d5235d490a8ff940e63e44f9f1" },
     };
     const res = await fetch(
-      `https://api.football-data.org/v4/competitions/PL`,
+      `http://api.football-data.org/v4/persons/44`,
       requestOptions
     );
     const json = await res.json();
     console.log(json);
+    setPerson(json);
   };
   return (
     <div>
+      <p className="text-center h1 text-white mt-5">{person.name}</p>
     </div>
   );
 };
